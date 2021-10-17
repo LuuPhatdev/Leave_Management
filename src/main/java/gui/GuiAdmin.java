e7cc6969e8cf2901c58d38ee745616de5e91c661
luuphat <luuphatdev@gmail.com>
2021-10-18 02:33:08 +0700

    create ui admin screen

--- src/main/java/gui/GuiAdmin.java
+++ src/main/java/gui/GuiAdmin.java
@@ -1,11 +1,24 @@
 package gui;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class GuiAdmin extends JFrame {
 
     private final String userName;
     private JPanel contentPane;
+    private JPanel sidePanel;
+    private JPanel centerPanel;
+    private JPanel p1;
+    private JPanel btn1;
+    private JPanel p2;
+    private JPanel btn2;
+    private JPanel btnLogOut;
+    private JPanel btnAddUser;
+    private JPanel btnUpdate;
+    private JPanel btnDelete;
 
     public GuiAdmin(String userName) {
         this.userName = userName;
@@ -15,6 +28,48 @@ public class GuiAdmin extends JFrame {
         setBounds(100, 100, 900, 637);
         setTitle("User Management");
         setVisible(true);
+
+        btn1.setCursor(new Cursor(Cursor.HAND_CURSOR));
+        btn1.addMouseListener(new MouseAdapter() {
+            public void mouseClicked(MouseEvent e) {
+                btn1ActionPerformed(e);
+            }
+        });
+
+        btn2.setCursor(new Cursor(Cursor.HAND_CURSOR));
+        btn2.addMouseListener(new MouseAdapter() {
+            public void mouseClicked(MouseEvent e) {
+                btn2ActionPerformed(e);
+                setCursor(new Cursor(Cursor.HAND_CURSOR));
+            }
+        });
+
+        btnLogOut.setCursor(new Cursor(Cursor.HAND_CURSOR));
+        btnLogOut.addMouseListener(new MouseAdapter() {
+            public void mouseClicked(MouseEvent e) {
+                btnLogOutActionPerformed(e);
+            }
+        });
     }
 
+    private void btn1ActionPerformed(MouseEvent e) {
+        p1.setVisible(true);
+        p2.setVisible(false);
+    }
+
+    private void btn2ActionPerformed(MouseEvent e) {
+        p1.setVisible(false);
+        p2.setVisible(true);
+    }
+
+    private void btnLogOutActionPerformed(MouseEvent e) {
+        int q = JOptionPane.showOptionDialog(null, "Are you sure exit?", "Exit", JOptionPane.YES_NO_OPTION,
+                JOptionPane.PLAIN_MESSAGE, null, null, null);
+
+        if (q == JOptionPane.YES_OPTION) {
+            this.dispose();
+            GuiLogin mainPane = new GuiLogin();
+            mainPane.setVisible(true);
+        }
+    }
 }

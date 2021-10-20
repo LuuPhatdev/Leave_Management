58eb1957ba64fb884ff28e8d4682bc5587e9a62c
BluXDra <leeyanxmen@gmail.com>
2021-10-20 14:57:22 +0700

    deduce annual leave

--- src/main/java/dao/EmployeeDao.java
+++ src/main/java/dao/EmployeeDao.java
@@ -54,4 +54,24 @@ public class EmployeeDao {
         }
         return employees.get(0);
     }
+    public void updateSingleEmployee(Employee employee){
+        try (var connect = ConnectDBProperty.getConnectionFromClassPath();
+             var cs = connect.prepareStatement("UPDATE employee SET dep_id = ?, fullname = ?, gender = ?, date_of_birth = ?, " +
+                     "phone = ?, email = ?, date_start = ?, annual_leave = ?, manager_id = ? " +
+                     "WHERE employee_id = ?");) {
+            cs.setInt(1, employee.getDepartmentId());
+            cs.setString(2, employee.getFullName());
+            cs.setInt(3, employee.getGender());
+            cs.setDate(4, Date.valueOf(employee.getDateOfBirth()));
+            cs.setString(5, employee.getPhone());
+            cs.setString(6, employee.getEmail());
+            cs.setDate(7, Date.valueOf(employee.getDateStart()));
+            cs.setDouble(8, employee.getAnnualLeave());
+            cs.setInt(9, employee.getManagerId());
+            cs.setInt(10, employee.getEmployeeId());
+            cs.executeUpdate();
+        } catch (Exception ex) {
+            JOptionPane.showMessageDialog(null, ex.getMessage());
+        }
+    }
 }

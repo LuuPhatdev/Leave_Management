36545a7aca9e9c55f439d26e4bc3f99a14429363
luuphat <luuphatdev@gmail.com>
2021-10-30 04:54:16 +0700

    add script

--- script.sql
+++ script.sql
@@ -178,5 +178,145 @@ end
 update [employee]
 set annual_leave = annual_leave + 1
 
+    USE [msdb]
+GO
+
+-- Update +1 date
+/****** Object:  Job [update_automatically]    Script Date: 10/30/2021 4:45:58 AM ******/
+BEGIN TRANSACTION
+DECLARE @ReturnCode INT
+SELECT @ReturnCode = 0
+/****** Object:  JobCategory [[Uncategorized (Local)]]    Script Date: 10/30/2021 4:45:58 AM ******/
+IF NOT EXISTS (SELECT name FROM msdb.dbo.syscategories WHERE name=N'[Uncategorized (Local)]' AND category_class=1)
+    BEGIN
+        EXEC @ReturnCode = msdb.dbo.sp_add_category @class=N'JOB', @type=N'LOCAL', @name=N'[Uncategorized (Local)]'
+        IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+
+    END
+
+DECLARE @jobId BINARY(16)
+EXEC @ReturnCode =  msdb.dbo.sp_add_job @job_name=N'update_automatically',
+                    @enabled=1,
+                    @notify_level_eventlog=0,
+                    @notify_level_email=0,
+                    @notify_level_netsend=0,
+                    @notify_level_page=0,
+                    @delete_level=0,
+                    @description=N'No description available.',
+                    @category_name=N'[Uncategorized (Local)]',
+                    @owner_login_name=N'sa', @job_id = @jobId OUTPUT
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+/****** Object:  Step [Step update]    Script Date: 10/30/2021 4:45:58 AM ******/
+EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'Step update',
+                   @step_id=1,
+                   @cmdexec_success_code=0,
+                   @on_success_action=1,
+                   @on_success_step_id=0,
+                   @on_fail_action=2,
+                   @on_fail_step_id=0,
+                   @retry_attempts=0,
+                   @retry_interval=0,
+                   @os_run_priority=0, @subsystem=N'TSQL',
+                   @command=N'use leave_management
+   update [employee]
+        set annual_leave = annual_leave + 1',
+                   @database_name=N'leave_management',
+                   @flags=0
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+EXEC @ReturnCode = msdb.dbo.sp_update_job @job_id = @jobId, @start_step_id = 1
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+EXEC @ReturnCode = msdb.dbo.sp_add_jobschedule @job_id=@jobId, @name=N'update_annualLeave',
+                   @enabled=1,
+                   @freq_type=32,
+                   @freq_interval=8,
+                   @freq_subday_type=1,
+                   @freq_subday_interval=0,
+                   @freq_relative_interval=1,
+                   @freq_recurrence_factor=1,
+                   @active_start_date=20211029,
+                   @active_end_date=99991231,
+                   @active_start_time=0,
+                   @active_end_time=235959,
+                   @schedule_uid=N'9bdce4a4-df98-4858-909d-9bb9713cc4fc'
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+EXEC @ReturnCode = msdb.dbo.sp_add_jobserver @job_id = @jobId, @server_name = N'(local)'
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+COMMIT TRANSACTION
+GOTO EndSave
+QuitWithRollback:
+IF (@@TRANCOUNT > 0) ROLLBACK TRANSACTION
+EndSave:
+GO
 
 
+-- Refresh after june
+USE [msdb]
+GO
+
+/****** Object:  Job [reset_annual_leave]    Script Date: 10/30/2021 4:49:21 AM ******/
+BEGIN TRANSACTION
+DECLARE @ReturnCode INT
+SELECT @ReturnCode = 0
+/****** Object:  JobCategory [[Uncategorized (Local)]]    Script Date: 10/30/2021 4:49:21 AM ******/
+IF NOT EXISTS (SELECT name FROM msdb.dbo.syscategories WHERE name=N'[Uncategorized (Local)]' AND category_class=1)
+    BEGIN
+        EXEC @ReturnCode = msdb.dbo.sp_add_category @class=N'JOB', @type=N'LOCAL', @name=N'[Uncategorized (Local)]'
+        IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+
+    END
+
+DECLARE @jobId BINARY(16)
+EXEC @ReturnCode =  msdb.dbo.sp_add_job @job_name=N'reset_annual_leave',
+                    @enabled=1,
+                    @notify_level_eventlog=0,
+                    @notify_level_email=0,
+                    @notify_level_netsend=0,
+                    @notify_level_page=0,
+                    @delete_level=0,
+                    @description=N'No description available.',
+                    @category_name=N'[Uncategorized (Local)]',
+                    @owner_login_name=N'sa', @job_id = @jobId OUTPUT
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+/****** Object:  Step [reset_annual_leave]    Script Date: 10/30/2021 4:49:22 AM ******/
+EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'reset_annual_leave',
+                   @step_id=1,
+                   @cmdexec_success_code=0,
+                   @on_success_action=1,
+                   @on_success_step_id=0,
+                   @on_fail_action=2,
+                   @on_fail_step_id=0,
+                   @retry_attempts=0,
+                   @retry_interval=0,
+                   @os_run_priority=0, @subsystem=N'TSQL',
+                   @command=N'use leave_management
+update [employee]
+set annual_leave = 6
+where annual_leave >=6
+go',
+                   @database_name=N'leave_management',
+                   @flags=0
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+EXEC @ReturnCode = msdb.dbo.sp_update_job @job_id = @jobId, @start_step_id = 1
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+EXEC @ReturnCode = msdb.dbo.sp_add_jobschedule @job_id=@jobId, @name=N'reset_annual_leave',
+                   @enabled=0,
+                   @freq_type=1,
+                   @freq_interval=0,
+                   @freq_subday_type=0,
+                   @freq_subday_interval=0,
+                   @freq_relative_interval=0,
+                   @freq_recurrence_factor=0,
+                   @active_start_date=20220601,
+                   @active_end_date=99991231,
+                   @active_start_time=0,
+                   @active_end_time=235959,
+                   @schedule_uid=N'c75069e2-0a51-4891-aaaf-912240133589'
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+EXEC @ReturnCode = msdb.dbo.sp_add_jobserver @job_id = @jobId, @server_name = N'(local)'
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+COMMIT TRANSACTION
+GOTO EndSave
+QuitWithRollback:
+IF (@@TRANCOUNT > 0) ROLLBACK TRANSACTION
+EndSave:
+GO

69652efbdefacb88492a50021f7d620b129763d5
BluXDra <leeyanxmen@gmail.com>
2021-10-30 10:10:05 +0700

    update add do not move a chief to another department

--- src/main/java/gui/Form/GuiUpdateEmloyee.java
+++ src/main/java/gui/Form/GuiUpdateEmloyee.java
@@ -2,6 +2,7 @@ package gui.Form;
 
 import com.toedter.calendar.JDateChooser;
 import com.toedter.calendar.JTextFieldDateEditor;
+import dao.DepartmentDao;
 import dao.EmployeeDao;
 import entity.Employee;
 import gui.GuiAdmin;
@@ -66,38 +67,50 @@ public class GuiUpdateEmloyee extends JFrame {
 
     private void btnUpdateEmployeeActionPerfomerd(ActionEvent e) {
             var dao = new EmployeeDao();
-            var employeeId = Integer.parseInt(txtEmployeeId.getText());
-            var departmentid = Integer.parseInt(txtDepartmentId.getText());
-            var fullName = txtFullname.getText();
-            var gender = Objects.requireNonNull(JcomboboxGender.getSelectedItem()).toString();
-            var dateOfBirth = JDateDateOfBirth.getDate().toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
-            var phone = txtPhone.getText();
-            var email = txtEmail.getText();
-            var dateStart = JDateStart.getDate().toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
-            var annualLeave = Double.parseDouble(txtAnnualLeave.getText());
-            var managerId = Integer.parseInt(txtManagerId.getText());
-            System.out.println(employeeId);
-            switch (gender) {
-                case "Male" -> gender = String.valueOf(1);
-                case "Female" -> gender = String.valueOf(0);
-                case "Other" -> gender = String.valueOf(-1);
-                default -> gender = ("Err");
+            var departmentDao = new DepartmentDao();
+            var count = 1;
+            while (count > 0 ){
+                var employeeId = Integer.parseInt(txtEmployeeId.getText());
+                var departmentid = 0;
+                if(departmentDao.checkIfIsChiefDepartment(employeeId)){
+                    JOptionPane.showMessageDialog(null, "you cant not move a chief of department to another department");
+                    break;
+                }else{
+                    departmentid = Integer.parseInt(txtDepartmentId.getText());
+                }
+                var fullName = txtFullname.getText();
+                var gender = Objects.requireNonNull(JcomboboxGender.getSelectedItem()).toString();
+                var dateOfBirth = JDateDateOfBirth.getDate().toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+                var phone = txtPhone.getText();
+                var email = txtEmail.getText();
+                var dateStart = JDateStart.getDate().toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+                var annualLeave = Double.parseDouble(txtAnnualLeave.getText());
+                var managerId = Integer.parseInt(txtManagerId.getText());
+                System.out.println(employeeId);
+                switch (gender) {
+                    case "Male" -> gender = String.valueOf(1);
+                    case "Female" -> gender = String.valueOf(0);
+                    case "Other" -> gender = String.valueOf(-1);
+                    default -> gender = ("Err");
+                }
+
+                Employee employee = new Employee();
+                employee.setEmployeeId(employeeId);
+                employee.setDepartmentId(departmentid);
+                employee.setFullName(fullName);
+                employee.setGender(Integer.parseInt(gender));
+                employee.setDateOfBirth(dateOfBirth);
+                employee.setPhone(phone);
+                employee.setEmail(email);
+                employee.setDateStart(dateStart);
+                employee.setAnnualLeave(annualLeave);
+                employee.setManagerId(managerId);
+                dao.updateSingleEmployee(employee);
+                guiAdmin.showListEmployee();
+                this.dispose();
+                count --;
             }
 
-            Employee employee = new Employee();
-            employee.setEmployeeId(employeeId);
-            employee.setDepartmentId(departmentid);
-            employee.setFullName(fullName);
-            employee.setGender(Integer.parseInt(gender));
-            employee.setDateOfBirth(dateOfBirth);
-            employee.setPhone(phone);
-            employee.setEmail(email);
-            employee.setDateStart(dateStart);
-            employee.setAnnualLeave(annualLeave);
-            employee.setManagerId(managerId);
-            dao.updateSingleEmployee(employee);
-            guiAdmin.showListEmployee();
-            this.dispose();
     }
 
 
